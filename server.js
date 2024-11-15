3f1978072c276587a2d9c890ff940210f96b9699
oqnd2 <oquendo2nd@gmail.com>
2024-11-15 00:35:03 -0500

    fix: Solucion error puerto del servidor

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
