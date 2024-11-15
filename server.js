d515542e9e64570a216a21c1a8d47b35581b608b
oqnd2 <oquendo2nd@gmail.com>
2024-11-15 00:18:19 -0500

    fix: Solución bug de bcrypt

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql2/promise");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const app = express();
